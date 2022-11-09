0c99adff36655f286caee4036aa475ef4318fe8c
ankita8vkaps <ankita.yadav@vkaps.com>
2022-11-09 11:41:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CustomCounter from './CustomCounter';
+
+function App() {
+  return (
+    <>
+    <CustomCounter/>
+    </>
+  );
+}
+
+export default App;
